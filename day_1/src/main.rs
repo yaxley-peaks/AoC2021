9b845e3085178c2222f96e176e35707643743afb
yaxley-peaks <epiclycoolgaemer@gmail.com>
2021-12-01 16:53:42 +0530

    all iterator implementation

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -4,13 +4,12 @@ use std::fs;
 fn d1_sum() {
     let inp = fs::read_to_string("./input.txt").unwrap();
     let nums: Vec<u32> = inp.lines().map(|v| v.parse::<u32>().unwrap()).collect();
-    let mut num = 0;
-    for x in 1..nums.len() {
-        if nums[x] > nums[x - 1] {
-            num += 1
-        }
-    }
-    println!("part1: {}", num);
+    let res = nums
+                        .iter()
+                        .tuple_windows()
+                        .filter(|(f, s)| f < s)
+                        .count();
+    println!("part1: {}", res);
 }
 
 fn d3_sum() {
