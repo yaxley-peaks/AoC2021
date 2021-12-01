8df039321f5d41b5474cafcc6812e2caa7d2e561
yaxley-peaks <epiclycoolgaemer@gmail.com>
2021-12-01 18:00:05 +0530

    addded benchmarking

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,32 +1,47 @@
 use itertools::Itertools;
 use std::fs;
+use std::time::Instant;
 
-fn d1_sum() {
-    let inp = fs::read_to_string("./input.txt").unwrap();
-    let nums: Vec<u32> = inp.lines().map(|v| v.parse::<u32>().unwrap()).collect();
-    let res = nums
-            .iter()
-            .tuple_windows()
-            .filter(|(f, s)| f < s)
-            .count();
-    println!("part1: {}", res);
+fn d1_sum(nums: &Vec<u32>) -> u32 {
+    nums.iter()
+        .tuple_windows()
+        .filter(|(f, s)| f < s)
+        .count()
+        .try_into()
+        .unwrap()
 }
-
-fn d3_sum() {
-    let inp = fs::read_to_string("./input.txt").unwrap();
-    let nums: Vec<u32> = inp.lines().map(|v| v.parse::<u32>().unwrap()).collect();
-
-    let res = nums
-        .iter()
+#[allow(dead_code)]
+fn d1_sum_noiter(nums: &Vec<u32>) -> u32 {
+    let mut num = 0;
+    for x in 1..nums.len() {
+        if nums[x] > nums[x - 1] {
+            num += 1
+        }
+    }
+    num
+}
+fn d3_sum(nums: &Vec<u32>) -> u32 {
+    nums.iter()
         .tuple_windows::<(_, _, _)>()
         .map(|(f, s, t)| f + s + t)
         .tuple_windows::<(_, _)>()
         .filter(|(f, s)| f < s)
-        .count();
-    println!("second part: {}", res);
+        .count()
+        .try_into()
+        .unwrap()
 }
-
+fn bench(f: fn(&Vec<u32>) -> u32, args: &Vec<u32>, msg: &str) {
+    let n = Instant::now();
+    f(args);
+    let time = n.elapsed().as_nanos();
+    println!("time elapsed since function call - {} :{} ", msg, time);
+}
 fn main() {
-    d1_sum();
-    d3_sum();
+    let inp = fs::read_to_string("./input.txt").unwrap();
+    let nums: Vec<u32> = inp.lines().map(|v| v.parse::<u32>().unwrap()).collect();
+
+    println!("part1: {}", d1_sum(&nums));
+    println!("part2: {}", d3_sum(&nums));
+    bench(d1_sum, &nums, "part 1 iter");
+    bench(d3_sum, &nums, "part 2 iter");
 }
