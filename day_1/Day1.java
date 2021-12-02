388becfaabdf1c7b732436ee77149302c9148510
yaxley-peaks <epiclycoolgaemer@gmail.com>
2021-12-02 21:36:49 +0530

    part 2 java

--- day_1/Day1.java
+++ day_1/Day1.java
@@ -5,28 +5,41 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Vector;
 
-/**
- * Day1
- */
 public class Day1 {
+
+    public static int part1(Vector<Integer> input) {
+        int sum = 0;
+        for (int i = 1; i < input.size(); i++) {
+            if (input.get(i) > input.get(i - 1)) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+    public static int part2(Vector<Integer> input){
+        int sum = 0;
+        for(int i = 3; i < input.size(); i++){
+            if(input.get(i) > input.get(i-3)){
+                sum++;
+            }}
+        return sum;
+        
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("D:\\Documents\\AoC2021\\day_1\\input.txt");
-        // File file = new File("input.txt");
+        // File file = new File("input.txt"); // why this no work??
         Scanner filScanner = new Scanner(file);
         Vector<Integer> input = new Vector<Integer>();
-        
-        int sum = 0;
+
         while (filScanner.hasNextLine()) {
             String line = filScanner.nextLine();
             input.add(Integer.parseInt(line));
         }
-        for(int i = 1; i < input.size(); i++){
-            if(input.get(i) > input.get(i-1)){
-                sum++;
-            }
-        }
-        System.out.println(sum);
         filScanner.close();
+
+        System.out.println(part1(input));
+        System.out.println(part2(input));
     }
 }
