638b52c6f4c778c513786c5e4039d49c7445db1b
yaxley-peaks <epiclycoolgaemer@gmail.com>
2021-12-02 11:51:44 +0530

    put all into one function and added benchmarking

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,8 +1,7 @@
 use itertools::Itertools;
 use std::fs;
 
-fn part1() {
-    let inp = fs::read_to_string("./input.txt").unwrap();
+fn solve(inp: String) -> (u32,u32) {
     let inp = inp
         .lines()
         .map(|x| x.split(" ").collect_tuple::<(_, _)>().unwrap())
@@ -11,54 +10,42 @@ fn part1() {
 
     let mut depth = 0;
     let mut length = 0;
+    let mut aim = 0;
+    let mut depth2 = 0;
 
     for mes in inp {
         match mes {
             ("forward", x) => {
                 length += x;
+                depth2 += aim * x;
             }
             ("up", x) => {
                 depth -= x;
-            }
-            ("down", x) => {
-                depth += x;
-            }
-            _ => {}
-        }
-    }
-    println!("{}", length * depth);
-}
-
-fn part2() {
-    let inp = fs::read_to_string("./input.txt").unwrap();
-    let inp = inp
-        .lines()
-        .map(|x| x.split(" ").collect_tuple::<(_, _)>().unwrap())
-        .map(|(f, s)| (f, s.parse::<u32>().unwrap()))
-        .collect::<Vec<(_, _)>>();
-
-    let mut length = 0;
-    let mut depth = 0;
-    let mut aim = 0;
-
-    for mes in inp {
-        match mes {
-            ("forward", x) => {
-                length += x;
-                depth += aim * x;
-            }
-            ("up", x) => {
                 aim -= x;
             }
             ("down", x) => {
+                depth += x;
                 aim += x;
             }
             _ => {}
         }
     }
-    println!("{}" , length*depth);
+    (length * depth, length * depth2)
 }
+
+fn bench<T,R>(f: fn(T) -> R, args: T, msg: &str) {
+    use std::time::Instant;
+    let n = Instant::now();
+    f(args);
+    let time = n.elapsed().as_nanos();
+    println!("time elapsed since function call - {} :{} ns ", msg, time);
+}
+
 fn main() {
-    part1();
-    part2();
+    let inp = fs::read_to_string("./input.txt").unwrap();
+    let (x,y) = solve(inp.clone());
+
+    bench(solve, inp.clone(), "part1");
+
+    println!("{} {}" , x, y);
 }
