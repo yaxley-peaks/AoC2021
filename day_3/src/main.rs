f0bbe81f514633d7bdd860b0749b893757bd7aa0
yaxley-peaks <epiclycoolgaemer@gmail.com>
2021-12-03 13:33:19 +0530

    ok this works now

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,8 +1,6 @@
-use itertools::Itertools;
 use std::fs;
-use transpose::transpose;
 
-fn part1(inp: &Vec<Vec<char>>) -> u128{
+fn part1_bad(inp: &Vec<Vec<char>>) -> u128 {
     let mut gamma = Vec::new();
     let mut epsilon = Vec::new();
 
@@ -33,18 +31,58 @@ fn part1(inp: &Vec<Vec<char>>) -> u128{
     let gms: String = gamma.iter().collect();
     let gms = isize::from_str_radix(&gms, 2).unwrap();
 
-    (gms*eps).try_into().unwrap()
+    (gms * eps).try_into().unwrap()
 }
 
+fn count_bits_at_pos(nums: &[u32], pos: usize) -> (usize, usize) {
+    let mut res = [0, 0];
+    for &num in nums {
+        res[(num as usize >> pos) & 1] += 1;
+    }
+    (res[0], res[1])
+}
+
+fn part1(nums: &[u32]) -> u32 {
+    let (mut x, mut y) = (0, 0);
+
+    for i in 0..12 {
+        let (zero, one) = count_bits_at_pos(&nums, i);
+        let temp = if one > zero { &mut x } else { &mut y };
+        *temp += 1 << i;
+    }
+
+    x * y
+}
+
+fn part2(nums: &[u32], a: u32, b: u32) -> u32 {
+    let mut nums = nums.to_vec();
+    for i in (0..12).rev() {
+        let (zero, one) = count_bits_at_pos(&nums, i);
+        let keep = if one >= zero { a } else { b };
+        nums.retain(|x| (x >> i) & 1 == keep);
+        if nums.len() == 1 {
+            break;
+        }
+    }
+    nums[0]
+}
 
 fn main() {
-    let inp = fs::read_to_string("./input.txt").unwrap();
-    let inp: Vec<_> = inp
+    let input = fs::read_to_string("./input.txt").unwrap();
+    let input: Vec<_> = input
         .lines()
         .into_iter()
         .map(|x| x.chars().collect::<Vec<_>>())
         .collect();
 
-    
+    let inp = fs::read_to_string("./input.txt").unwrap();
+    let inp = inp
+        .lines()
+        .map(|line| u32::from_str_radix(line, 2).unwrap())
+        .collect::<Vec<_>>();
+
     println!("{}", part1(&inp));
+    println!("{}", part2(&inp, 1, 0) * part2(&inp, 0, 1));
+
+    println!("{}", part1_bad(&input));
 }
